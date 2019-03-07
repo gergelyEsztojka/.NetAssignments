953f6ede5eb9407dcc906bb8614c7a7c99404571
gergelyEsztojka <esztojka.gergely@gmail.com>
2019-03-07 14:02:02 +0100

    All prectice finished before WPF

--- CreateClass/CreateClass/Program.cs
+++ CreateClass/CreateClass/Program.cs
@@ -10,6 +10,24 @@ namespace CreateClass
     {
         static void Main(string[] args)
         {
+            Person p1 = new Person { Name = "Adam", BirthDate = DateTime.Now, Gender = Genders.Male };
+            Person p2 = new Person { Name = "Eva", BirthDate = DateTime.Now, Gender = Genders.Female };
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+            Employee e1 = new Employee { Name = "Bob", BirthDate = DateTime.Now, Gender = Genders.Male, Profession = "Accountant", Salary = 1200.75m };
+
+            Console.WriteLine(e1);
+
+            Employee Kovacs = new Employee { Name = "Géza", BirthDate = DateTime.Now, Salary = 1000m, Profession = "léhűtő" };
+            Kovacs.Room = new Room { RoomNum = 111 };
+            Employee Kovacs2 = (Employee)Kovacs.Clone();
+            Kovacs2.Room.RoomNum = 112;
+            Console.WriteLine(Kovacs.ToString());
+            Console.WriteLine(Kovacs2.ToString());
+
+            Console.ReadLine();
         }
     }
 }
