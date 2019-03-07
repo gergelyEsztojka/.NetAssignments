83bf696e5ee0ab374b2d484b9949f9ecc01bdb98
gergelyEsztojka <esztojka.gergely@gmail.com>
2019-03-07 17:43:35 +0100

    WPF Command Line practice

---
+++ WPFCommandLine/WPFCommandLine/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFCommandLine
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string[] Args;
+
+        void app_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length == 0) return;
+
+            if (e.Args.Length > 0)
+            {
+                Args = e.Args;
+            }
+        }
+    }
+}
