f345c3d167a9cc60f8e29c61518f159835396c0c
gergelyEsztojka <esztojka.gergely@gmail.com>
2019-03-07 18:05:49 +0100

    WPF Data Binding practice

---
+++ WPFDataBinding/WPFDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFDataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person = new Person { Name = "Jane", Age = 26 };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = person;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = person.Name + " is " + person.Age;
+            MessageBox.Show(message);
+        }
+    }
+
+    public class Person
+    {
+        private string NameValue;
+
+        public string Name
+        {
+            get { return NameValue; }
+            set { NameValue = value; }
+        }
+
+        private double AgeValue;
+
+        public double Age
+        {
+            get { return AgeValue; }
+            set
+            {
+                if(value != AgeValue)
+                {
+                    AgeValue = value;
+                }
+            }
+        }
+    }
+}
