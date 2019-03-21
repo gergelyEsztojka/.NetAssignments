3aeceafe8db5b78dc20611a50ab66f267894bf69
gergelyEsztojka <esztojka.gergely@gmail.com>
2019-03-21 17:11:53 +0100

    SeekAndArchive program is finished?!

--- SI2Assignments/LookupCollections/Program.cs
+++ SI2Assignments/LookupCollections/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Specialized;
 using System.Globalization;
 
@@ -8,12 +9,19 @@ namespace LookupCollections
     {
         static void Main(string[] args)
         {
-            var ListDict = new ListDictionary
+            var ListDict = new ListDictionary(StringComparer.OrdinalIgnoreCase)
             {
                 { "Estados Unidos", "United States" },
                 { "Canadá", "Canada" },
                 { "España", "Spain" }
             };
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("es-ES");
+
+            Console.WriteLine(ListDict["Canadá"]);
+            Console.WriteLine(ListDict["España"]);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ SI2Assignments/SeekAndArchive/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace SeekAndArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo searchedFile = new FileInfo(args[0]);
+            DirectoryInfo searchedDirectory = new DirectoryInfo(args[1]);
+
+            Console.WriteLine(searchedFile.FullName);
+
+            FileInfo datFile = new FileInfo(@"C:\Users\Gery\source\repos\AdvanceSI\DotNet\SI_Assignments\TestingFolder\testingDat.dat");
+
+            BinaryReader br = new BinaryReader(datFile.OpenRead());
+            
+            if(searchedFile.LastWriteTime.ToString() == br.ReadString())
+            {
+                br.Close();
+                Console.WriteLine("File has not been changed from last time");
+            }
+            else
+            {
+                br.Close();
+                BinaryWriter bw = new BinaryWriter(datFile.OpenWrite());
+
+                bw.Write(searchedFile.LastWriteTime.ToString());
+                bw.Close();
+                Compress(searchedFile.FullName, searchedFile.FullName.Replace(".txt", ".gzip"));
+            }
+
+            
+
+            Console.ReadLine();
+        }
+
+        static void Compress(string fileToCompress, string outputFileName)
+        {
+            using (FileStream inputStream = new FileStream(fileToCompress, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                using (FileStream outputStream = new FileStream(outputFileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    using (GZipStream gzip = new GZipStream(outputStream, CompressionMode.Compress))
+                    {
+                        inputStream.CopyTo(gzip);
+                    }
+                }
+            }
+        }
+    }
+}
